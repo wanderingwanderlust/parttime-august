7b208f9655711abf76d8d7a0197a23f28bb0129b
Greg <gohan@Gregs-MBP-2.attlocal.net>
2023-04-03 17:48:32 -0400

    application tier

---
+++ part3/server/server.js
@@ -0,0 +1,51 @@
+import express, { application } from "express";
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb+srv://root:root@giphyapi.o3wilzu.mongodb.net/?retryWrites=true&w=majority');
+
+const app = express();
+const port = 3001 // react frontend app will run on port 3000
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+const db = {
+    items: [
+        {
+            item: 'bread',
+            cost: 5
+        },
+        {
+            item: 'peanut butter',
+            cost: 20
+        },
+        {
+            item: 'jelly',
+            cost: 8
+        },
+    ],
+    users: [
+        {
+            username: 'Greg',
+            password: 'password'
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send('<html><body><h1>Hello World by Greg Lindeman</h1></body></html>')
+})
+
+app.get('/users', (req, res) => {
+    res.json(db.users)
+})
+
+app.get('/items', (req, res) => {
+    res.json(db.items)
+})
+
+app.listen(port, () =>{
+    console.log(`app is listening on port ${port}`)
+})
+
+
